ab70ab17af1d13eb84564eba9a933b5fbfb88f84
FelipeCostaq <felipecostasiqu@gmail.com>
2025-08-25 19:32:47 -0300

    :alien: ParkingSpotController Getters

---
+++ Controllers/ParkingSpotController.cs
@@ -0,0 +1,65 @@
+﻿using carParkingApi.Context;
+using carParkingApi.Enum;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carParkingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingSpotController : ControllerBase
+    {
+        private readonly ParkingContext _context;
+
+        public ParkingSpotController(ParkingContext context)
+        {
+            _context = context;
+        }
+
+        #region Get
+
+        [HttpGet("{id}")]
+        public IActionResult GetSpotById(int id)
+        {
+            var spot = _context.ParkingSpots.FirstOrDefault(p => p.Id == id);
+
+            if (spot == null)
+                return NotFound();
+
+            return Ok(spot);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var spots = _context.ParkingSpots.ToList();
+
+            return Ok(spots);
+        }
+
+        [HttpGet("number")]
+        public IActionResult GetByNumber(int number)
+        {
+            var spots = _context.ParkingSpots.Where(x => x.Number == number).ToList();
+
+            return Ok(spots);
+        }
+
+        [HttpGet("type")]
+        public IActionResult GetByType(string type)
+        {
+            var spots = _context.ParkingSpots.Where(x => x.Type != null && x.Type.Contains(type)).ToList();
+
+            return Ok(spots);
+        }
+
+        [HttpGet("status")]
+        public IActionResult GetByStatus(ParkingSpotState status)
+        {
+            var spots = _context.ParkingSpots.Where(s => s.Status == status).ToList();
+
+            return Ok(spots);
+        }
+
+        #endregion
+    }
+}
