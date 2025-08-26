266c5a7dd9591c86ba3e5c34b3bb31d1dc05dd4a
FelipeCostaq <felipecostasiqu@gmail.com>
2025-08-26 20:12:43 -0300

    :alien: ParkingSpotController All and GetById

---
+++ Controllers/ParkingAssignmentController.cs
@@ -0,0 +1,38 @@
+﻿using carParkingApi.Context;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carParkingApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controlller]")]
+    public class ParkingAssignmentController : ControllerBase
+    {
+        private readonly ParkingContext _context;
+
+        public ParkingAssignmentController(ParkingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllParkingAssignments()
+        {
+            var parkingAssignments = _context.ParkingAssignments.ToList();
+
+            return Ok(parkingAssignments);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var parkingAssignment = _context.ParkingAssignments.FirstOrDefault(p => p.Id == id);
+
+            if (parkingAssignment == null)
+                return NotFound();
+
+            return Ok(parkingAssignment);
+        }
+
+    }
+}
